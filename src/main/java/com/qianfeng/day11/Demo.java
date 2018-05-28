482834c26cd4f2cbd5059bdd25984bb851826eca
85800621 <85800621@qq.com>
2018-05-28 23:23:03 +0800

    提交测试

--- src/main/java/com/qianfeng/day11/Demo.java
+++ src/main/java/com/qianfeng/day11/Demo.java
@@ -6,6 +6,6 @@ public class Demo {
         System.out.println("第一个GitHub项目");
         System.out.println("第一次修改代码");
         System.out.println("修改SSH KEY测试上传");
-        System.out.println("check");
+        System.out.println("check1");
     }
 }
